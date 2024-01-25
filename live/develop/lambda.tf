754373011969ca99eeb728707cd4857aeb279c4f
Paul Moros Anacona <paul.moros@globant.com>
2024-01-25 13:30:48 -0500

    refactor: change path for lambda call

--- live/develop/lambda.tf
+++ live/develop/lambda.tf
@@ -18,7 +18,7 @@ resource "aws_api_gateway_rest_api" "example_api" {
 resource "aws_api_gateway_resource" "example_api_resource" {
   rest_api_id = aws_api_gateway_rest_api.example_api.id
   parent_id   = aws_api_gateway_rest_api.example_api.root_resource_id
-  path_part   = "${var.app_name}_${var.environment}"
+  path_part   = "hello"
 }
 
 resource "aws_api_gateway_method" "example_api_method_hello" {
